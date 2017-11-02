a9479688750b474a84d39b736db35b73a0588b49
Oksana2254 <luginaooksana@ukr.net>
2017-11-02 22:37:09 +0200

    сверстала часть блока

--- app/js/init.js
+++ app/js/init.js
@@ -38,9 +38,53 @@ $( function() {
     modalBtn.on('click', function (e) {
         e.preventDefault();
         var target = $(this).attr('data-target');
-        $(target).bPopup();
-    })
+        $(target).bPopup({
+            closeClass: 'close'
+        });
+    });
 
     //form styler init
     $('select').styler();
+
+    //scrollbar init
+    $('.scrollbar-inner').scrollbar();
+
+    //chart init
+
+    var chartBlock = document.getElementById('myChart');
+    if(chartBlock){
+        var myChart = new Chart(chartBlock, {
+            type : 'bar',
+            data : {
+                labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09'],
+                datasets: [
+                    {
+                        label: "Signups",
+                        data: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+                        backgroundColor: 'rgba(128, 191, 202, 1)'
+                    },
+                    {
+                        label: "FTD",
+                        data: [100, 270, 300, 101, 500, 600, 350, 800, 900],
+                        backgroundColor: 'rgba(255, 154, 56, 1)'
+                    },
+                    {
+                        label: "FTD",
+                        data: [100, 270, 300, 101, 500, 600, 350, 800, 900],
+                        backgroundColor: '#59b66d'
+                    }
+                ]
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        })
+    }
 } );
